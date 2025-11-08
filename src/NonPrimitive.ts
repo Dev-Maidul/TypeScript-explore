0e01a0e76e8e7898625854b641bd09a6fc903ea6
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-11-09 00:31:00 +0800

    Learn Non primitive data in ts

---
+++ src/NonPrimitive.ts
@@ -0,0 +1,8 @@
+let shoppingList:string[]=["egg","Milk","Sugar"];
+shoppingList.push("Apple");
+
+let mixArray:(string|number)[]=["Maidul",11,];
+mixArray.push(33);
+
+//! Tuple
+let destination :[string,string,number]=["Dhaka","Beijing",5];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 const course:string="web development";
 
 console.log(course);
+
+let userId:number;
+userId=123;
+
+let isAdmin:boolean;
+isAdmin=false;
+
+let name;
+name=123;
+name="maidul";
+name=true;
+name=false;
