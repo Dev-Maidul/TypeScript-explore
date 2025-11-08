7fb1f4826afac08f1da641fd54cc43ea60b15da6
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-11-09 00:00:56 +0800

    install typescript

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const course = "web development";
+console.log(course);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const course:string="web development";
+
+console.log(course);
